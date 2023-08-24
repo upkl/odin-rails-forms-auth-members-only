11ab77f412d535fec6f6388f05f6c46a03f1f3f2
upkl <2737384+upkl@users.noreply.github.com>
2023-08-24 22:20:40 +0200

    added some views added display of usernames for logged-in users solved exercise in Ruby on Rails Course → Forms and Authentication → Project: Members Only!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,25 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @user = User.find(current_user.id)
+    @post = @user.posts.new(post_params)
+
+    if @post.save
+      redirect_to posts_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
